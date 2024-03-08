05db640dd4f9a889824ec92d17eac97308551373
Awad-Safwat <awad.safwat1999@gmail.com>
2024-03-08 09:56:33 +0300

    added app background

---
+++ fatatni_salah_app/lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const deepDark = Color(0xff17191D);
+  static const liteDark = Color(0xff2C3137);
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const green = Colors.green;
+  static const gray = Color(0xff858585);
+  static const silver = Color(0xffbdbdbd);
+  static const red = Colors.red;
+}
